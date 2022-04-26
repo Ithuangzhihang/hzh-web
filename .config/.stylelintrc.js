77a7314bd81a2833e0a895606e25c9366c35167c
yyjb5 <yyjb5@163.com>
2022-04-26 22:00:28 +0800

    feat: 框架搭建

---
+++ .config/.stylelintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier', // Make sure this is always the last element in the array.
+  ],
+  plugins: ['stylelint-order'],
+  rules: {
+    "selector-class-pattern": [ // 命名规范 -
+      "^(([A-Z]|[a-z])[a-z0-9]*)(-[a-z0-9]+)*$",
+      {
+        "message": "首字母可大写的横线命名"
+      }
+    ],
+    "keyframes-name-pattern": [ // 命名规范 -
+      "^(([A-Z]|[a-z])[a-z0-9]*)(-[a-z0-9]+)*$",
+      {
+        "message": "首字母可大写的横线命名"
+      }
+    ],
+    'block-no-empty': null,
+    'no-empty-source': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
+};
---
+++ src/@types/main.d.ts
@@ -0,0 +1,17 @@
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.json';
+declare module '*.css';
+declare module '*.less';
+
+declare module '@jiaminghi/data-view-react';
+declare module 'postcss-pxtorem';
+declare module 'less-vars-to-js';
+
+/** vite 打包环境变量 */
+declare const VITE_BUILD_MODE: 'dev' | 'test' | 'prod';
