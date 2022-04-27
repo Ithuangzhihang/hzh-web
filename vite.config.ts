dce76b98a279317b0d86ccb2f0d857cd52a4b03e
yyjb5 <yyjb5@163.com>
2022-04-27 07:23:00 +0000

    fix: 修改less插件

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,22 @@ import stylelintPlugin from './plugins/vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin(),
-    stylelintPlugin(),
-  ],
+  plugins: [react(), eslintPlugin(), stylelintPlugin()],
+  css: {
+    // css modules
+    modules: {
+      scopeBehaviour: 'local',
+      generateScopedName: '[local]_[hash:base64:6]',
+    },
+    postcss: {
+      plugins: [],
+    },
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
