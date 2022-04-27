c8fe156e0de8ccb4b77964a13471d369250e4d77
yyjb5 <yyjb5@163.com>
2022-04-27 06:18:25 +0000

    fix: 添加vite执行eslint和stylelint插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+import eslintPlugin from './plugins/vite-plugin-eslint';
+import stylelintPlugin from './plugins/vite-plugin-stylelint';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin(),
+    stylelintPlugin(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
