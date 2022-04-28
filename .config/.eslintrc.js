d502107abe26497959ab1285f23d27f1d8026584
yyjb5 <yyjb5@163.com>
2022-04-28 05:40:20 +0000

    fix: 增加eslint柜子

--- .config/.eslintrc.js
+++ .config/.eslintrc.js
@@ -40,5 +40,49 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/consistent-type-imports': ['error'],
+    // 允许使用async
+    'generator-star-spacing': 0,
+    'babel/generator-star-spacing': 0,
+    '@typescript-eslint/no-var-requires': 2,
+    '@typescript-eslint/type-annotation-spacing': 2,
+    // 开发环境换行符
+    'linebreak-style': [0, 'windows'],
+    // 三等号
+    eqeqeq: [2, 'always'],
+    // 注释符号和注释内容之间必须有空格
+    'spaced-comment': [2, 'always'],
+    // 关键字后面使用一致的空格
+    'keyword-spacing': 2,
+    // 强制在 function的左括号之前使用一致的空格
+    'space-before-function-paren': 0,
+    // 不以新行开始的块前面要不要有空格
+    'space-before-blocks': 2,
+    // 引号类型
+    quotes: [1, 'single'],
+    // 禁止出现未使用过的变量
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': [2],
+    // 要求或禁止末尾逗号
+    'comma-dangle': 0,
+    // js语句结尾必须使用分号
+    semi: [2, 'always'],
+    'array-bracket-spacing': [2, 'never'],
+    'react/jsx-filename-extension': [0, { extensions: ['.tsx', '.ts'] }],
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': [0],
+    'max-classes-per-file': [2, 2],
+    'no-console': "warn",
+    'max-len': ['error', { code: 100, ignoreTemplateLiterals: true }],
+    'no-param-reassign': ['error', { props: false }],
+    'react/prop-types': [0],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
