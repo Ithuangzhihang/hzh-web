40aef88ecc9c31565d92ce39b1883994680a805f
yyjb5 <yyjb5@163.com>
2022-05-24 21:45:46 +0800

    feat: 添加graphql，apolloClient,及错误捕获

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import './index.css';
 
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { apolloClient } from '@/apis/client';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
